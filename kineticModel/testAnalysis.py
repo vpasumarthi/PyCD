e9ed636a8c9e65245afc5df37ae8fbf7d6de4076
Viswanath Pasumarthi <pasumart@buffalo.edu>
2016-09-19 18:00:09 -0400

    new project title, report print
    Project title changed from kMC_Project to kineticModel
    Report Printing and data saving enabled.

---
+++ kineticModel/testAnalysis.py
@@ -0,0 +1,20 @@
+from kineticModel import analysis
+import numpy as np
+
+trajectoryData = np.load('trajectoryData_1electron_PBC_1e03KMCSteps_1e02PathSteps_1Traj.npy')
+
+nStepsMSD = 5E+01
+nDispMSD = 5E+01
+binsize = 10E+00
+maxBinSize = 1 # ns
+reprTime = 'ns'
+reprDist = 'Angstrom'
+
+hematiteAnalysis = analysis(trajectoryData[()], nStepsMSD, nDispMSD, binsize, maxBinSize, reprTime, reprDist)
+
+timeArray = trajectoryData[()].timeArray
+unwrappedPositionArray = trajectoryData[()].unwrappedPositionArray
+msdAnalysisData = hematiteAnalysis.computeMSD(timeArray, unwrappedPositionArray)
+msdData = msdAnalysisData.msdData
+speciesTypes = msdAnalysisData.speciesTypes
+hematiteAnalysis.displayMSDPlot(msdData, speciesTypes, save=1)
