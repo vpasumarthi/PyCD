fixed broadcasting shape errors for ESPConfig

SourceCode/KineticModel.py
@@ -626,7 +626,7 @@ class system(object):
         ESPConfig = np.zeros((self.neighbors.numSystemElements, 1))
         for elementIndex in range(self.neighbors.numSystemElements):
             neighborIndices = self.neighborSystemElementIndexMap[elementIndex].keys()
-            ESPConfig[elementIndex] = np.sum(self.inverseCoeffDistanceList[elementIndex] * currentStateChargeConfig[neighborIndices])
+            ESPConfig[elementIndex] = np.dot(self.inverseCoeffDistanceList[elementIndex], currentStateChargeConfig[neighborIndices])
         return ESPConfig
     
     #@profile
@@ -833,10 +833,8 @@ class run(object):
                                 delG0List.append(delG0)
                             else:
                                 columnIndex = self.system.neighborSystemElementIndexMap[speciesSiteSystemElementIndex][neighborSiteSystemElementIndex]
-                                #delG0 = 0
                                 delG0 = (self.speciesChargeList[speciesIndex] * ((currentStateESPConfig[neighborSiteSystemElementIndex][0] - currentStateESPConfig[speciesSiteSystemElementIndex][0]
                                                                                   - self.speciesChargeList[speciesIndex] * self.system.inverseCoeffDistanceList[speciesSiteSystemElementIndex][columnIndex])))
-                                import pdb; pdb.set_trace()
                             if excess:
                                 delG0List.append(delG0)
                             lambdaValue = self.nProcLambdaValueList[iProc]
@@ -870,8 +868,8 @@ class run(object):
                 else:
                     oldSiteNeighbors = self.system.neighborSystemElementIndexMap[oldSiteSystemElementIndex].keys()
                     newSiteNeighbors = self.system.neighborSystemElementIndexMap[newSiteSystemElementIndex].keys()
-                    currentStateESPConfig[oldSiteNeighbors] -= self.speciesChargeList[speciesIndex] * self.system.inverseCoeffDistanceList[oldSiteSystemElementIndex]
-                    currentStateESPConfig[newSiteNeighbors] += self.speciesChargeList[speciesIndex] * self.system.inverseCoeffDistanceList[newSiteSystemElementIndex]
+                    currentStateESPConfig[oldSiteNeighbors] -= self.speciesChargeList[speciesIndex] * self.system.inverseCoeffDistanceList[oldSiteSystemElementIndex][:, None]
+                    currentStateESPConfig[newSiteNeighbors] += self.speciesChargeList[speciesIndex] * self.system.inverseCoeffDistanceList[newSiteSystemElementIndex][:, None]
                 if (step + 1) % stepInterval == 0:
                     timeArray[pathIndex] = kmcTime
                     unwrappedPositionArray[pathIndex] = unwrappedPositionArray[pathIndex - 1] + speciesDisplacementVectorList