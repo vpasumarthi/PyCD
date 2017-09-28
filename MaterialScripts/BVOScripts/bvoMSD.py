e26ac5ce3734c00bbcde016d3cf3649c19200832
Viswanath Pasumarthi <pasumart@buffalo.edu>
2017-09-28 04:07:53 -0400

    modified BVOScripts compatible with PEP8

--- MaterialScripts/BVOScripts/bvoMSD.py
+++ MaterialScripts/BVOScripts/bvoMSD.py
@@ -1,30 +1,39 @@
 #!/usr/bin/env python
 
-def bvoMSD(systemSize, pbc, nDim, Temp, speciesCount, tFinal, nTraj, timeInterval, 
-                msdTFinal, trimLength, displayErrorBars, reprTime, reprDist, report, overWrite):
+import os
+import platform
+import pickle
+from KineticModel import analysis
 
-    from KineticModel import analysis
-    import numpy as np
-    import os
-    import platform
-    import pickle
-    
-    # Determine path for system directory    
+directorySeparator = '\\' if platform.uname()[0] == 'Windows' else '/'
+
+
+def bvoMSD(systemSize, pbc, nDim, Temp, speciesCount, tFinal, nTraj,
+           timeInterval, msdTFinal, trimLength, displayErrorBars, reprTime,
+           reprDist, report):
+
+    # Determine path for system directory
     cwd = os.path.dirname(os.path.realpath(__file__))
-    directorySeparator = '\\' if platform.uname()[0]=='Windows' else '/'
-    nLevelUp = 3 if platform.uname()[0]=='Linux' else 4
-    systemDirectoryPath = directorySeparator.join(cwd.split(directorySeparator)[:-nLevelUp] + 
-                                                  ['KineticModelSimulations', 'BVO', ('PBC' if pbc else 'NoPBC'), 
-                                                   ('SystemSize' + str(systemSize).replace(' ', ''))])
+    nLevelUp = 3 if platform.uname()[0] == 'Linux' else 4
+    systemDirectoryPath = directorySeparator.join(
+                        cwd.split(directorySeparator)[:-nLevelUp]
+                        + ['KineticModelSimulations', 'BVO',
+                           ('PBC' if pbc else 'NoPBC'),
+                           ('SystemSize' + str(systemSize).replace(' ', ''))])
 
     # Change to working directory
     parentDir1 = 'SimulationFiles'
     nElectrons = speciesCount[0]
     nHoles = speciesCount[1]
-    parentDir2 = str(nElectrons) + ('electron' if nElectrons==1 else 'electrons') + ', ' + str(nHoles) + ('hole' if nHoles==1 else 'holes')
+    parentDir2 = (str(nElectrons)
+                  + ('electron' if nElectrons == 1 else 'electrons') + ', '
+                  + str(nHoles) + ('hole' if nHoles == 1 else 'holes'))
     parentDir3 = str(Temp) + 'K'
-    workDir = (('%1.2E' % tFinal) + 'SEC,' + ('%1.2E' % timeInterval) + 'TimeInterval,' + ('%1.2E' % nTraj) + 'Traj')
-    workDirPath = systemDirectoryPath + directorySeparator + directorySeparator.join([parentDir1, parentDir2, parentDir3, workDir])
+    workDir = (('%1.2E' % tFinal) + 'SEC,' + ('%1.2E' % timeInterval)
+               + 'TimeInterval,' + ('%1.2E' % nTraj) + 'Traj')
+    workDirPath = (systemDirectoryPath + directorySeparator
+                   + directorySeparator.join([parentDir1, parentDir2,
+                                              parentDir3, workDir]))
     if not os.path.exists(workDirPath):
         print 'Simulation files do not exist. Aborting.'
     else:
@@ -32,27 +41,33 @@ def bvoMSD(systemSize, pbc, nDim, Temp, speciesCount, tFinal, nTraj, timeInterva
 
         # Determine path for neighbor list directories
         inputFileDirectoryName = 'InputFiles'
-        inputFileDirectoryPath = systemDirectoryPath + directorySeparator + inputFileDirectoryName
-    
+        inputFileDirectoryPath = (systemDirectoryPath
+                                  + directorySeparator
+                                  + inputFileDirectoryName)
+
         # Build path for material and neighbors object files
         materialName = 'bvo'
         tailName = '.obj'
-        directorySeparator = '\\' if platform.uname()[0]=='Windows' else '/'
         objectFileDirectoryName = 'ObjectFiles'
-        objectFileDirPath = inputFileDirectoryPath + directorySeparator + objectFileDirectoryName
-        materialFileName = objectFileDirPath + directorySeparator + materialName + tailName
-        
+        objectFileDirPath = (inputFileDirectoryPath
+                             + directorySeparator
+                             + objectFileDirectoryName)
+        materialFileName = (objectFileDirPath + directorySeparator
+                            + materialName + tailName)
+
         # Load material object
         file_bvo = open(materialFileName, 'r')
         bvo = pickle.load(file_bvo)
         file_bvo.close()
-    
-        bvoAnalysis = analysis(bvo, nDim, systemSize, speciesCount, nTraj, tFinal, 
-                                    timeInterval, msdTFinal, trimLength, reprTime, reprDist)
-        
+
+        bvoAnalysis = analysis(bvo, nDim, systemSize, speciesCount, nTraj,
+                               tFinal, timeInterval, msdTFinal, trimLength,
+                               reprTime, reprDist)
+
         msdAnalysisData = bvoAnalysis.computeMSD(workDirPath, report)
         msdData = msdAnalysisData.msdData
         stdData = msdAnalysisData.stdData
         speciesTypes = msdAnalysisData.speciesTypes
         fileName = msdAnalysisData.fileName
-        bvoAnalysis.generateMSDPlot(msdData, stdData, displayErrorBars, speciesTypes, fileName, workDirPath)
+        bvoAnalysis.generateMSDPlot(msdData, stdData, displayErrorBars,
+                                    speciesTypes, fileName, workDirPath)
--- MaterialScripts/BVOScripts/bvoSetup.py
+++ MaterialScripts/BVOScripts/bvoSetup.py
@@ -1,70 +1,101 @@
 #!/usr/bin/env python
-def bvoSetup(systemSize, pbc, generateObjectFiles, generateHopNeighborList, 
-                  generateCumDispList, alpha, nmax, kmax, generatePrecomputedArray):
-    """Prepare material class object file, neighborlist and saves to the provided destination path"""
-    from bvoParameters import bvoParameters
-    from KineticModel import material, neighbors, system
-    import numpy as np
-    import platform
-    import os
-    
-    # Determine path for system directory    
+
+import os
+import platform
+import numpy as np
+from KineticModel import material, neighbors, system
+from bvoParameters import bvoParameters
+
+directorySeparator = '\\' if platform.uname()[0] == 'Windows' else '/'
+
+
+def bvoSetup(systemSize, pbc, generateObjectFiles, generateHopNeighborList,
+             generateCumDispList, alpha, nmax, kmax, generatePrecomputedArray):
+    """Prepare material class object file, neighborlist and \
+        saves to the provided destination path"""
+
+    # Determine path for system directory
     cwd = os.path.dirname(os.path.realpath(__file__))
-    directorySeparator = '\\' if platform.uname()[0]=='Windows' else '/'
-    nLevelUp = 3 if platform.uname()[0]=='Linux' else 4
-    systemDirectoryPath = directorySeparator.join(cwd.split(directorySeparator)[:-nLevelUp] + 
-                                                  ['KineticModelSimulations', 'BVO', ('PBC' if np.all(pbc) else 'NoPBC'), 
-                                                   ('SystemSize[' + ','.join(['%i' % systemSize[i] for i in range(len(systemSize))]) + ']')])
+
+    nLevelUp = 3 if platform.uname()[0] == 'Linux' else 4
+    systemDirectoryPath = directorySeparator.join(
+        cwd.split(directorySeparator)[:-nLevelUp]
+        + ['KineticModelSimulations', 'BVO',
+           ('PBC' if np.all(pbc) else 'NoPBC'),
+           ('SystemSize[' + ','.join(['%i' % systemSize[i]
+                                      for i in range(len(systemSize))]) + ']')]
+                                                  )
 
     # Determine path for input files
     inputFileDirectoryName = 'InputFiles'
-    inputFileDirectoryPath = systemDirectoryPath + directorySeparator + inputFileDirectoryName
-    
+    inputFileDirectoryPath = (systemDirectoryPath
+                              + directorySeparator
+                              + inputFileDirectoryName)
+
     # Build path for material and neighbors object files
     materialName = 'bvo'
     tailName = '.obj'
     objectFileDirectoryName = 'ObjectFiles'
-    objectFileDirPath = inputFileDirectoryPath + directorySeparator + objectFileDirectoryName
+    objectFileDirPath = (inputFileDirectoryPath
+                         + directorySeparator
+                         + objectFileDirectoryName)
     if not os.path.exists(objectFileDirPath):
         os.makedirs(objectFileDirPath)
-    materialFileName = objectFileDirPath + directorySeparator + materialName + tailName
-    neighborsFileName = objectFileDirPath + directorySeparator + materialName + 'Neighbors' + tailName
+    materialFileName = (objectFileDirPath + directorySeparator
+                        + materialName + tailName)
+    neighborsFileName = (objectFileDirPath + directorySeparator + materialName
+                         + 'Neighbors' + tailName)
 
     # Build material object files
     bvo = material(bvoParameters())
 
     # Build neighbors object files
     bvoNeighbors = neighbors(bvo, systemSize, pbc)
-            
+
     if generateObjectFiles:
         bvo.generateMaterialFile(bvo, materialFileName)
         bvoNeighbors.generateNeighborsFile(bvoNeighbors, neighborsFileName)
-    
+
     # generate neighbor list
     if generateHopNeighborList:
-        bvoNeighbors.generateNeighborList(inputFileDirectoryPath, generateCumDispList)
+        bvoNeighbors.generateNeighborList(inputFileDirectoryPath,
+                                          generateCumDispList)
 
     # Build precomputed array and save to disk
-    precomputedArrayFilePath = inputFileDirectoryPath + directorySeparator + 'precomputedArray.npy'
+    precomputedArrayFilePath = (inputFileDirectoryPath
+                                + directorySeparator
+                                + 'precomputedArray.npy')
     if generatePrecomputedArray:
         # Load input files to instantiate system class
         os.chdir(inputFileDirectoryPath)
-        hopNeighborListFileName = inputFileDirectoryPath + directorySeparator + 'hopNeighborList.npy'
+        hopNeighborListFileName = (inputFileDirectoryPath
+                                   + directorySeparator
+                                   + 'hopNeighborList.npy')
         hopNeighborList = np.load(hopNeighborListFileName)[()]
-        cumulativeDisplacementListFilePath = inputFileDirectoryPath + directorySeparator + 'cumulativeDisplacementList.npy'
-        cumulativeDisplacementList = np.load(cumulativeDisplacementListFilePath)
-        
-        # Note: speciesCount doesn't effect the precomputedArray, however it is essential to instantiate system class
+        cumulativeDisplacementListFilePath = (
+                                            inputFileDirectoryPath
+                                            + directorySeparator
+                                            + 'cumulativeDisplacementList.npy')
+        cumulativeDisplacementList = np.load(
+                                            cumulativeDisplacementListFilePath)
+
+        # Note: speciesCount doesn't effect the precomputedArray,
+        # however it is essential to instantiate system class
         # So, any non-zero value of speciesCount will do.
         nElectrons = 1
         nHoles = 0
         speciesCount = np.array([nElectrons, nHoles])
 
-        bvoSystem = system(bvo, bvoNeighbors, hopNeighborList, cumulativeDisplacementList, 
-                                speciesCount, alpha, nmax, kmax)
+        bvoSystem = system(bvo, bvoNeighbors, hopNeighborList,
+                           cumulativeDisplacementList, speciesCount,
+                           alpha, nmax, kmax)
         precomputedArray = bvoSystem.ewaldSumSetup(inputFileDirectoryPath)
         np.save(precomputedArrayFilePath, precomputedArray)
-        
-        ewaldParametersFilePath = inputFileDirectoryPath + directorySeparator + 'ewaldParameters.npy'
-        ewaldParameters = {'alpha': alpha, 'nmax' : nmax, 'kmax' : kmax}
+
+        ewaldParametersFilePath = (inputFileDirectoryPath
+                                   + directorySeparator
+                                   + 'ewaldParameters.npy')
+        ewaldParameters = {'alpha': alpha,
+                           'nmax': nmax,
+                           'kmax': kmax}
         np.save(ewaldParametersFilePath, ewaldParameters)
