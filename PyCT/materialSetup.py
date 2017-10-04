3e012dc4b54e8027db940dbd61db057f6ab62775
Viswanath Pasumarthi <pasumart@buffalo.edu>
2017-10-03 22:56:55 -0400

    inputs config files from ConfigDir; avoid platform import

--- PyCT/materialSetup.py
+++ PyCT/materialSetup.py
@@ -1,84 +1,63 @@
 #!/usr/bin/env python
 
 import os
-import platform
 
 import numpy as np
+import yaml
 
 from PyCT.core import material, neighbors, system
 
-directorySeparator = '\\' if platform.uname()[0] == 'Windows' else '/'
 
-
-def materialSetup(inputCoorFileLocation, materialParameters, systemSize, pbc,
-                  generateObjectFiles, generateHopNeighborList,
-                  generateCumDispList, alpha, nmax, kmax,
-                  generatePrecomputedArray):
+def materialSetup(systemDirectoryPath, systemSize, pbc, generateObjectFiles,
+                  generateHopNeighborList, generateCumDispList, alpha, nmax,
+                  kmax, generatePrecomputedArray):
     """Prepare material class object file, neighborlist and \
         saves to the provided destination path"""
 
-    # [latticeMatrix, elementTypes, nElementsPerUnitCell,
-    #  fractionalUnitCellCoords] = readPOSCAR(inputCoorFileLocation)
-    # nElementTypes = len(elementTypes)
-    # elementTypeIndexList = np.repeat(np.arange(nElementTypes),
-    #                                 nElementsPerUnitCell)
-    # unitcellCoords = np.dot(latticeMatrix, fractionalUnitCellCoords.T).T
-    # materialParameters.__dict__.update({'latticeMatrix': latticeMatrix,
-    #                                    'elementTypes': elementTypes,
-    #                                   'nElementsPerUnitCell':
-    #                                   nElementsPerUnitCell,
-    #                                   'unitcellCoords':
-    #                                   unitcellCoords,
-    #                                   'elementTypeIndexList':
-    #                                   elementTypeIndexList})
+    # Load material parameters
+    configDirName = 'ConfigurationFiles'
+    configFileName = 'sysconfig.yml'
+    configFilePath = os.path.join(systemDirectoryPath, configDirName,
+                                  configFileName)
+    with open(configFilePath, 'r') as stream:
+        try:
+            params = yaml.load(stream)
+        except yaml.YAMLError as exc:
+            print exc
 
-    materialParameters.__dict__.update({'inputCoorFileLocation':
-                                        inputCoorFileLocation})
+    inputCoordinateFileName = 'POSCAR'
+    inputCoorFileLocation = os.path.join(systemDirectoryPath, configDirName,
+                                         inputCoordinateFileName)
+    params.update({'inputCoorFileLocation': inputCoorFileLocation})
+    materialParameters = returnValues(params)
+
     # Build material object files
     bvo = material(materialParameters)
-    # sortedKeyList = sorted(bvo.__dict__.keys())
-    # print [(key, bvo.__dict__[key]) for key in sortedKeyList]
-    # import pdb; pdb.set_trace()
     materialName = bvo.name
 
     # Build neighbors object files
     bvoNeighbors = neighbors(bvo, systemSize, pbc)
 
-    # Determine path for system directory
-    cwd = os.path.dirname(os.path.realpath(__file__))
-
-    nLevelUp = 3 if platform.uname()[0] == 'Linux' else 3
-    systemDirectoryPath = directorySeparator.join(
-        cwd.split(directorySeparator)[:-nLevelUp]
-        + ['PyCTSimulations', materialName,
-           ('PBC' if np.all(pbc) else 'NoPBC'),
-           ('SystemSize[' + ','.join(['%i' % systemSize[i]
-                                      for i in range(len(systemSize))]) + ']')]
-                                                  )
-
     # Determine path for input files
     inputFileDirectoryName = 'InputFiles'
-    inputFileDirectoryPath = (systemDirectoryPath
-                              + directorySeparator
-                              + inputFileDirectoryName)
+    inputFileDirectoryPath = os.path.join(systemDirectoryPath,
+                                          inputFileDirectoryName)
 
     # Build path for material and neighbors object files
     tailName = '.obj'
     objectFileDirectoryName = 'ObjectFiles'
-    objectFileDirPath = (inputFileDirectoryPath
-                         + directorySeparator
-                         + objectFileDirectoryName)
+    objectFileDirPath = os.path.join(inputFileDirectoryPath,
+                                     objectFileDirectoryName)
     if not os.path.exists(objectFileDirPath):
         os.makedirs(objectFileDirPath)
 
-    materialFileName = (objectFileDirPath + directorySeparator
-                        + materialName + tailName)
-    neighborsFileName = (objectFileDirPath + directorySeparator + materialName
+    materialFilePath = os.path.join(objectFileDirPath, materialName) + tailName
+    neighborsFilePath = (os.path.join(objectFileDirPath, materialName)
                          + 'Neighbors' + tailName)
 
     if generateObjectFiles:
-        bvo.generateMaterialFile(bvo, materialFileName)
-        bvoNeighbors.generateNeighborsFile(bvoNeighbors, neighborsFileName)
+        bvo.generateMaterialFile(bvo, materialFilePath)
+        bvoNeighbors.generateNeighborsFile(bvoNeighbors, neighborsFilePath)
 
     # generate neighbor list
     if generateHopNeighborList:
@@ -86,20 +65,17 @@ def materialSetup(inputCoorFileLocation, materialParameters, systemSize, pbc,
                                           generateCumDispList)
 
     # Build precomputed array and save to disk
-    precomputedArrayFilePath = (inputFileDirectoryPath
-                                + directorySeparator
-                                + 'precomputedArray.npy')
+    precomputedArrayFilePath = os.path.join(inputFileDirectoryPath,
+                                            'precomputedArray.npy')
     if generatePrecomputedArray:
         # Load input files to instantiate system class
         os.chdir(inputFileDirectoryPath)
-        hopNeighborListFileName = (inputFileDirectoryPath
-                                   + directorySeparator
-                                   + 'hopNeighborList.npy')
+        hopNeighborListFileName = os.path.join(inputFileDirectoryPath,
+                                               'hopNeighborList.npy')
         hopNeighborList = np.load(hopNeighborListFileName)[()]
-        cumulativeDisplacementListFilePath = (
-                                            inputFileDirectoryPath
-                                            + directorySeparator
-                                            + 'cumulativeDisplacementList.npy')
+        cumulativeDisplacementListFilePath = os.path.join(
+                                            inputFileDirectoryPath,
+                                            'cumulativeDisplacementList.npy')
         cumulativeDisplacementList = np.load(
                                             cumulativeDisplacementListFilePath)
 
@@ -116,10 +92,17 @@ def materialSetup(inputCoorFileLocation, materialParameters, systemSize, pbc,
         precomputedArray = bvoSystem.ewaldSumSetup(inputFileDirectoryPath)
         np.save(precomputedArrayFilePath, precomputedArray)
 
-        ewaldParametersFilePath = (inputFileDirectoryPath
-                                   + directorySeparator
-                                   + 'ewaldParameters.npy')
+        ewaldParametersFilePath = os.path.join(inputFileDirectoryPath,
+                                               'ewaldParameters.npy')
         ewaldParameters = {'alpha': alpha,
                            'nmax': nmax,
                            'kmax': kmax}
         np.save(ewaldParametersFilePath, ewaldParameters)
+
+
+class returnValues(object):
+    """dummy class to return objects from methods \
+        defined inside other classes"""
+    def __init__(self, inputdict):
+        for key, value in inputdict.items():
+            setattr(self, key, value)
