1274651b725c05f189537de8e6e8a183fe5f65b3
Viswanath Pasumarthi <pasumart@buffalo.edu>
2017-10-06 00:20:38 -0400

    add example set for BVO

---
+++ examples/BVO/RunFiles/Run.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+import os.path
+
+import numpy as np
+
+from PyCT.materialRun import materialRun
+
+# Frequently modified input parameters:
+nElectrons = 1
+tFinal = 1.00E-07
+nTraj = 1.00E+02
+timeInterval = 1.00E-08
+
+# Input parameters:
+Temp = 300  # K
+nHoles = 0
+speciesCount = np.array([nElectrons, nHoles])
+randomSeed = 2
+report = 1
+overWrite = 1
+systemDirectoryPath = os.path.dirname(
+                                os.path.dirname(os.path.realpath(__file__)))
+materialRun(systemDirectoryPath, Temp, speciesCount, tFinal, nTraj,
+            timeInterval, randomSeed, report, overWrite)
---
+++ examples/BVO/generateInputFiles.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+import os.path
+
+import numpy as np
+
+from PyCT.materialSetup import materialSetup
+
+# Input parameters:
+systemSize = np.array([2, 2, 1])
+pbc = np.array([1, 1, 1])
+generateObjectFiles = 1
+generateHopNeighborList = 1
+generateCumDispList = 1
+alpha = 0.18
+nmax = 0
+kmax = 4
+generatePrecomputedArray = 1
+systemDirectoryPath = os.path.dirname(os.path.realpath(__file__))
+
+materialSetup(systemDirectoryPath, systemSize, pbc, generateObjectFiles,
+              generateHopNeighborList, generateCumDispList, alpha, nmax, kmax,
+              generatePrecomputedArray)
