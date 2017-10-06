de85cb93070fed78bbeb1ea33278a55ec236d782
Viswanath Pasumarthi <pasumart@buffalo.edu>
2017-10-05 20:00:35 -0400

    PEP8 compliant

--- setup.py
+++ setup.py
@@ -45,5 +45,4 @@ if __name__ == "__main__":
             'Programming Language :: Python :: 2.7',
             'Programming Language :: Python :: 3',
         ],
-        zip_safe=True,
-)
+        zip_safe=True)
