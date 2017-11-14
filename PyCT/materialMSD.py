3d48c4f22fd5971df8c01f5b3a3240b716b11329
'Viswanath <'pasumart@buffalo.edu'>
2017-11-14 00:46:10 -0500

    input/output folder structuring out of PyCT; fixes #17

--- PyCT/materialMSD.py
+++ PyCT/materialMSD.py
@@ -5,10 +5,9 @@ import yaml
 from PyCT.core import material, analysis
 
 
-def materialMSD(inputDirectoryPath, systemSize, pbc, nDim, Temp, ionChargeType,
-                speciesChargeType, speciesCount, tFinal, nTraj, timeInterval,
-                msdTFinal, trimLength, displayErrorBars, reprTime, reprDist,
-                report):
+def materialMSD(inputDirectoryPath, dstPath, nDim, speciesCount, tFinal, nTraj,
+                timeInterval, msdTFinal, trimLength, displayErrorBars,
+                reprTime, reprDist, report):
 
     # Load material parameters
     configFileName = 'sysconfig.yml'
@@ -28,36 +27,17 @@ def materialMSD(inputDirectoryPath, systemSize, pbc, nDim, Temp, ionChargeType,
     # Build material object files
     materialInfo = material(materialParameters)
 
-    # Change to working directory
-    parentDir1 = 'SimulationFiles'
-    parentDir2 = ('ionChargeType=' + ionChargeType
-                  + '; speciesChargeType=' + speciesChargeType)
-    nElectrons = speciesCount[0]
-    nHoles = speciesCount[1]
-    parentDir3 = (str(nElectrons)
-                  + ('electron' if nElectrons == 1 else 'electrons') + ', '
-                  + str(nHoles) + ('hole' if nHoles == 1 else 'holes'))
-    parentDir4 = str(Temp) + 'K'
-    workDir = (('%1.2E' % tFinal) + 'SEC,' + ('%1.2E' % timeInterval)
-               + 'TimeInterval,' + ('%1.2E' % nTraj) + 'Traj')
-    systemDirectoryPath = inputDirectoryPath.resolve().parent
-    workDirPath = systemDirectoryPath.joinpath(parentDir1, parentDir2,
-                                               parentDir3, parentDir4, workDir)
+    materialAnalysis = analysis(materialInfo, nDim, speciesCount,
+                                nTraj, tFinal, timeInterval, msdTFinal,
+                                trimLength, reprTime, reprDist)
 
-    if not workDirPath.exists():
-        print('Simulation files do not exist. Aborting.')
-    else:
-        materialAnalysis = analysis(materialInfo, nDim, speciesCount,
-                                    nTraj, tFinal, timeInterval, msdTFinal,
-                                    trimLength, reprTime, reprDist)
-
-        msdAnalysisData = materialAnalysis.computeMSD(workDirPath, report)
-        msdData = msdAnalysisData.msdData
-        stdData = msdAnalysisData.stdData
-        speciesTypes = msdAnalysisData.speciesTypes
-        fileName = msdAnalysisData.fileName
-        materialAnalysis.generateMSDPlot(msdData, stdData, displayErrorBars,
-                                         speciesTypes, fileName, workDirPath)
+    msdAnalysisData = materialAnalysis.computeMSD(dstPath, report)
+    msdData = msdAnalysisData.msdData
+    stdData = msdAnalysisData.stdData
+    speciesTypes = msdAnalysisData.speciesTypes
+    fileName = msdAnalysisData.fileName
+    materialAnalysis.generateMSDPlot(msdData, stdData, displayErrorBars,
+                                     speciesTypes, fileName, dstPath)
     return None
 
 
--- PyCT/materialRun.py
+++ PyCT/materialRun.py
@@ -1,16 +1,14 @@
 #!/usr/bin/env python
 
-from pathlib import Path
-
 import numpy as np
 import yaml
 
 from PyCT.core import material, neighbors, system, run
 
 
-def materialRun(inputDirectoryPath, systemSize, pbc, Temp, ionChargeType,
-                speciesChargeType, speciesCount, tFinal, nTraj, timeInterval,
-                randomSeed, report, overWrite):
+def materialRun(inputDirectoryPath, dstPath, systemSize, pbc, Temp,
+                ionChargeType, speciesChargeType, speciesCount, tFinal, nTraj,
+                timeInterval, randomSeed, report, overWrite):
 
     # Load material parameters
     configFileName = 'sysconfig.yml'
@@ -33,25 +31,8 @@ def materialRun(inputDirectoryPath, systemSize, pbc, Temp, ionChargeType,
     # Build neighbors object files
     materialNeighbors = neighbors(materialInfo, systemSize, pbc)
 
-    # Change to working directory
-    parentDir1 = 'SimulationFiles'
-    parentDir2 = ('ionChargeType=' + ionChargeType
-                  + '; speciesChargeType=' + speciesChargeType)
-    nElectrons = speciesCount[0]
-    nHoles = speciesCount[1]
-    parentDir3 = (str(nElectrons)
-                  + ('electron' if nElectrons == 1 else 'electrons') + ', '
-                  + str(nHoles) + ('hole' if nHoles == 1 else 'holes'))
-    parentDir4 = str(Temp) + 'K'
-    workDir = (('%1.2E' % tFinal) + 'SEC,' + ('%1.2E' % timeInterval)
-               + 'TimeInterval,' + ('%1.2E' % nTraj) + 'Traj')
-    systemDirectoryPath = inputDirectoryPath.resolve().parent
-    workDirPath = systemDirectoryPath.joinpath(parentDir1, parentDir2,
-                                               parentDir3, parentDir4, workDir)
-    Path.mkdir(workDirPath, parents=True, exist_ok=True)
-
     fileExists = 0
-    if workDirPath.joinpath('Run.log').exists():
+    if dstPath.joinpath('Run.log').exists():
         fileExists = 1
     if not fileExists or overWrite:
         # Load input files to instantiate system class
@@ -77,7 +58,7 @@ def materialRun(inputDirectoryPath, systemSize, pbc, Temp, ionChargeType,
         materialRun = run(materialSystem, precomputedArray, Temp,
                           ionChargeType, speciesChargeType, nTraj, tFinal,
                           timeInterval)
-        materialRun.doKMCSteps(workDirPath, report, randomSeed)
+        materialRun.doKMCSteps(dstPath, report, randomSeed)
     else:
         print ('Simulation files already exists in '
                + 'the destination directory')
