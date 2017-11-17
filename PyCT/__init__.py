02c86aefbf417579b02c8c8aa9e59f15ad58080d
'Viswanath <'pasumart@buffalo.edu'>
2017-11-16 21:24:28 -0500

    remove unnecessary imports in PyCT package init

--- PyCT/__init__.py
+++ PyCT/__init__.py
@@ -1,13 +1,3 @@
 """
 The primary init of the project
 """
-
-from . import core
-from . import io
-from . import materialSetup
-from . import materialRun
-from . import materialMSD
-
-from . import math
-from .math import mult
-from .math import power
