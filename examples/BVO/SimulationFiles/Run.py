aac9ed04ffb1881ba30fefe4c1fc71b3cfde3cb1
Viswanath Pasumarthi <pasumart@buffalo.edu>
2017-12-02 15:44:14 -0500

    update examples

--- examples/BVO/SimulationFiles/Run.py
+++ examples/BVO/SimulationFiles/Run.py
@@ -2,35 +2,7 @@
 
 from pathlib import Path
 
-import numpy as np
-
 from PyCT.materialRun import materialRun
 
-# Frequently modified input parameters:
-nElectrons = 1
-tFinal = 1.00E-07
-nTraj = 1.00E+02
-timeInterval = 1.00E-08
-ionChargeType = 'full'
-speciesChargeType = 'full'
-
-# Input parameters:
-systemSize = np.array([2, 2, 1])
-pbc = np.array([1, 1, 1])
-Temp = 300  # K
-nHoles = 0
-speciesCount = np.array([nElectrons, nHoles])
-randomSeed = 2
-report = 1
-overWrite = 1
-
-# input/output directories
 dstPath = Path.cwd()
-nLevelUp = 0
-systemDirectoryPath = dstPath.resolve().parents[nLevelUp]
-inputFileDirectoryName = 'InputFiles'
-inputDirectoryPath = systemDirectoryPath.joinpath(inputFileDirectoryName)
-
-materialRun(inputDirectoryPath, dstPath, systemSize, pbc, Temp, ionChargeType,
-            speciesChargeType, speciesCount, tFinal, nTraj, timeInterval,
-            randomSeed, report, overWrite)
+materialRun(dstPath)
--- examples/BVO/SimulationFiles/generateMSDPlot.py
+++ examples/BVO/SimulationFiles/generateMSDPlot.py
@@ -2,35 +2,7 @@
 
 from pathlib import Path
 
-import numpy as np
-
 from PyCT.materialMSD import materialMSD
 
-# Frequently modified input parameters:
-nElectrons = 1
-tFinal = 1.00E-07
-nTraj = 1.00E+02
-timeInterval = 1.00E-08
-msdTFinal = 60  # in units of reprTime
-trimLength = 1
-ionChargeType = 'full'
-speciesChargeType = 'full'
-
-# Input parameters:
-nDim = 3
-Temp = 300  # K
-nHoles = 0
-speciesCount = np.array([nElectrons, nHoles])
-displayErrorBars = 1
-reprTime = 'ns'
-reprDist = 'angstrom'
-report = 1
 dstPath = Path.cwd()
-nLevelUp = 0
-systemDirectoryPath = dstPath.resolve().parents[nLevelUp]
-inputFileDirectoryName = 'InputFiles'
-inputDirectoryPath = systemDirectoryPath.joinpath(inputFileDirectoryName)
-
-materialMSD(inputDirectoryPath, dstPath, nDim, speciesCount, tFinal, nTraj,
-            timeInterval, msdTFinal, trimLength, displayErrorBars, reprTime,
-            reprDist, report)
+materialMSD(dstPath)
