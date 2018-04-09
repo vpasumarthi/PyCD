c6878c490f420c8b0656a9aaa40454452f36533c
'Viswanath <'pasumart@buffalo.edu'>
2018-04-09 13:37:33 -0400

    modify DataProfile input variables

--- PyCT/scripts/data_profile.py
+++ PyCT/scripts/data_profile.py
@@ -9,24 +9,19 @@ import matplotlib.pyplot as plt
 
 class DataProfile(object):
     def __init__(self, dst_path, system_directory_path,
-                 profiling_species_type_index, non_var_species_count,
-                 var_species_count_list, ion_charge_type, species_charge_type,
-                 temp, t_final, time_interval, n_traj, external_field):
+                 variable_quantity_type_index, variable_quantity_index,
+                 variable_quantity_list, species_count,
+                 ion_charge_type, species_charge_type, temp, t_final,
+                 time_interval, n_traj, external_field):
         self.dst_path = dst_path
         self.system_directory_path = system_directory_path
-        self.profiling_species_type_index = profiling_species_type_index
-        self.var_species_type = (
-            'electron' if self.profiling_species_type_index == 0 else 'hole')
-        self.non_var_species_count = non_var_species_count
-        self.var_species_count_list = var_species_count_list
-        self.profile_length = len(self.var_species_count_list)
-        self.species_count_list = np.zeros((self.profile_length, 2), int)
-        if self.profiling_species_type_index == 0:
-            self.species_count_list[:, 0] = self.var_species_count_list
-            self.species_count_list[:, 1] = self.non_var_species_count
-        else:
-            self.species_count_list[:, 0] = self.non_var_species_count
-            self.species_count_list[:, 1] = self.var_species_count_list
+        self.variable_quantity_type_index = variable_quantity_type_index
+        self.variable_quantity_index = variable_quantity_index
+        self.variable_quantity_list = variable_quantity_list
+        self.species_count = species_count
+        if self.variable_quantity_type_index == 1:
+            self.var_species_type = 'electron' if self.species_count[0] else 'hole'
+        self.num_runs = len(variable_quantity_list)
         self.ion_charge_type = ion_charge_type
         self.species_charge_type = species_charge_type
         self.temp = temp
@@ -80,8 +75,8 @@ class DataProfile(object):
         filename = (
             self.var_species_type + '_' + profiling_quantity + '_profile_'
             + self.ion_charge_type[0] + self.species_charge_type[0]
-            + '_' + str(self.var_species_count_list[0])
-            + '-' + str(self.var_species_count_list[-1])
+            + '_' + str(self.variable_quantity_list[0])
+            + '-' + str(self.variable_quantity_list[-1])
             + '_' + self.field_tag)
         figure_name = filename + '.png'
         figure_path = self.dst_path / figure_name
@@ -90,24 +85,28 @@ class DataProfile(object):
         return filename
 
     def diffusion_profile(self, plot_error_bars, msd_t_final, repr_time):
-        diffusivity_profile_data = np.zeros((self.profile_length, 3))
-        diffusivity_profile_data[:, 0] = np.copy(self.var_species_count_list)
+        diffusivity_profile_data = np.zeros((self.num_runs, 3))
+        diffusivity_profile_data[:, 0] = np.copy(self.variable_quantity_list)
         file_name = '%1.2E%s' % (msd_t_final, repr_time)
         msd_analysis_log_file_name = (
             'MSD_Analysis' + ('_' if file_name else '') + file_name + '.log')
 
-        for index in range(self.profile_length):
-            species_count = self.species_count_list[index, :]
+        for i_run in range(self.num_runs):
+            species_count = self.species_count
+            if self.variable_quantity_type_index == 1:
+                species_count[self.variable_quantity_index] = self.variable_quantity_list[i_run]
+
             work_dir_path = self.generate_work_dir_path(species_count)
             msd_analysis_log_file_path = (work_dir_path
                                           / msd_analysis_log_file_name)
             with open(msd_analysis_log_file_path, 'r') as msd_analysis_log_file:
                 first_line = msd_analysis_log_file.readline()
                 second_line = msd_analysis_log_file.readline()
-            diffusivity_profile_data[index, 1] = float(first_line[-13:-6])
-            diffusivity_profile_data[index, 2] = float(second_line[-13:-6])
+            diffusivity_profile_data[i_run, 1] = float(first_line[-13:-6])
+            diffusivity_profile_data[i_run, 2] = float(second_line[-13:-6])
 
         y_label = 'Diffusivity (${{\mu}}m^2/s$)'
+        species_count[self.variable_quantity_index] = self.variable_quantity_list[i_run]
         figure_title = ('Diffusion coefficient as a function of number of '
                         + self.var_species_type + 's')
         profiling_quantity = 'diffusion'
@@ -120,19 +119,22 @@ class DataProfile(object):
         return None
 
     def drift_mobility_profile(self, plot_error_bars):
-        drift_mobility_profile_data = np.zeros((self.profile_length, 3))
-        drift_mobility_profile_data[:, 0] = np.copy(self.var_species_count_list)
+        drift_mobility_profile_data = np.zeros((self.num_runs, 3))
+        drift_mobility_profile_data[:, 0] = np.copy(self.variable_quantity_list)
         run_log_file_name = 'Run.log'
 
-        for index in range(self.profile_length):
-            species_count = self.species_count_list[index, :]
+        for i_run in range(self.num_runs):
+            species_count = self.species_count
+            if self.variable_quantity_type_index == 1:
+                species_count[self.variable_quantity_index] = self.variable_quantity_list[i_run]
+
             work_dir_path = self.generate_work_dir_path(species_count)
             msd_analysis_log_file_path = work_dir_path / run_log_file_name
             with open(msd_analysis_log_file_path, 'r') as msd_analysis_log_file:
                 first_line = msd_analysis_log_file.readline()
                 second_line = msd_analysis_log_file.readline()
-            drift_mobility_profile_data[index, 1] = float(first_line[-19:-10])
-            drift_mobility_profile_data[index, 2] = float(second_line[-19:-10])
+            drift_mobility_profile_data[i_run, 1] = float(first_line[-19:-10])
+            drift_mobility_profile_data[i_run, 2] = float(second_line[-19:-10])
 
         y_label = 'Drift mobility ($cm^2/V.s$)'
         figure_title = ('Drift mobility as a function of number of '
@@ -147,12 +149,15 @@ class DataProfile(object):
         return None
 
     def runtime_profile(self):
-        elapsed_seconds_data = np.zeros((self.profile_length, 2))
-        elapsed_seconds_data[:, 0] = np.copy(self.var_species_count_list)
+        elapsed_seconds_data = np.zeros((self.num_runs, 2))
+        elapsed_seconds_data[:, 0] = np.copy(self.variable_quantity_list)
         run_log_file_name = 'Run.log'
 
-        for index in range(self.profile_length):
-            species_count = self.species_count_list[index, :]
+        for i_run in range(self.num_runs):
+            species_count = self.species_count
+            if self.variable_quantity_type_index == 1:
+                species_count[self.variable_quantity_index] = self.variable_quantity_list[i_run]
+
             work_dir_path = self.generate_work_dir_path(species_count)
             run_log_file_path = work_dir_path / run_log_file_name
             with open(run_log_file_path, 'r') as run_log_file:
@@ -169,7 +174,7 @@ class DataProfile(object):
                 num_seconds = float(first_line[36:38])
             elapsed_time = timedelta(days=num_days, hours=num_hours,
                                      minutes=num_minutes, seconds=num_seconds)
-            elapsed_seconds_data[index, 1] = int(elapsed_time.total_seconds())
+            elapsed_seconds_data[i_run, 1] = int(elapsed_time.total_seconds())
 
         y_label = 'Run Time (sec)'
         figure_title = ('Simulation run time as a function of number of '
