333681a870415f640bd41d8e5ffffdc400a382c3
'Viswanath <'pasumart@buffalo.edu'>
2019-07-18 13:51:21 -0400

    change all length and volume parameters from bohr to angstrom units

--- PyCT/core.py
+++ PyCT/core.py
@@ -786,12 +786,12 @@ class System(object):
     def minimize_real_space_cutoff_error(self, charge_list_einsum, real_space_parameters, x_real_initial_guess):
         if 'alpha' in real_space_parameters:
             alpha = real_space_parameters['alpha']
-            real_space_cutoff_error = lambda r_cut: charge_list_einsum * np.sqrt(r_cut / (2 * self.system_volume)) * (np.exp(-(alpha * r_cut)**2) / (alpha * r_cut)**2) - self.err_tol
+            real_space_cutoff_error = lambda r_cut: charge_list_einsum * np.sqrt(r_cut / (2 * self.system_volume / constants.ANG2BOHR**3)) * (np.exp(-(alpha * r_cut)**2) / (alpha * r_cut)**2) - self.err_tol
             r_cut0 = x_real_initial_guess / alpha 
             real_space_parameters['r_cut'] = fsolve(real_space_cutoff_error, r_cut0)[0]
         else:
             r_cut = real_space_parameters['r_cut']
-            real_space_cutoff_error = lambda alpha: charge_list_einsum * np.sqrt(r_cut / (2 * self.system_volume)) * (np.exp(-(alpha * r_cut)**2) / (alpha * r_cut)**2) - self.err_tol
+            real_space_cutoff_error = lambda alpha: charge_list_einsum * np.sqrt(r_cut / (2 * self.system_volume / constants.ANG2BOHR**3)) * (np.exp(-(alpha * r_cut)**2) / (alpha * r_cut)**2) - self.err_tol
             alpha0 = x_real_initial_guess / r_cut
             real_space_parameters['alpha'] = fsolve(real_space_cutoff_error, alpha0)[0]
         return real_space_parameters
@@ -840,7 +840,7 @@ class System(object):
 
         x_real_initial_guess = 0.5
         x_fourier_initial_guess = 0.5
-        volume_derived_length = np.power(self.system_volume, 1/3)
+        volume_derived_length = np.power(self.system_volume / constants.ANG2BOHR**3, 1/3)
         if not np.isreal(self.alpha) & np.isreal(self.r_cut) & np.isreal(self.k_cut):
             if np.isreal(self.alpha) & np.isreal(self.r_cut):
                 # optimize fourier-space cutoff error for k_cut
