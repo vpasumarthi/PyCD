f301cc32937d82097ade7f5396573a4a2cde4be9
'Viswanath <'pasumart@buffalo.edu'>
2019-07-23 22:42:18 -0400

    define get_convergence_rcut function

--- PyCT/core.py
+++ PyCT/core.py
@@ -850,6 +850,28 @@ class System(object):
             convergence_status = 0
         return convergence_status
 
+    def get_convergence_rcut(self, charge_list_prod, alpha, r_cut_max):
+        # analyze real-space energy convergence by varying r_cut
+        lower_bound = 0.50
+        upper_bound = 0.9999
+        num_data_points = 1.00E+01
+
+        r_cut_lower = lower_bound * r_cut_max
+        r_cut_upper = upper_bound * r_cut_max
+        r_cut_data = np.linspace(r_cut_lower, r_cut_upper, num_data_points)
+        real_space_energy_data = np.zeros(int(num_data_points))
+        for r_cut_index, r_cut in enumerate(r_cut_data):
+            precomputed_array_real = self.get_precomputed_array_real(alpha, r_cut)[0]
+            real_space_energy_data[r_cut_index] = np.sum(np.multiply(charge_list_prod, precomputed_array_real))
+
+        real_space_energy_deviation = np.abs(real_space_energy_data - real_space_energy_data[-1])
+        indices_of_non_convergence = np.where(real_space_energy_deviation > self.err_tol)[0]
+        if len(indices_of_non_convergence) == 0:
+            r_cut_convergence = r_cut_data[0]
+        else:
+            r_cut_convergence = r_cut_data[indices_of_non_convergence.max() + 1]
+        return r_cut_convergence
+
     def get_cutoff_parameters(self, tau_ratio):
         real_space_parameters = {}
         fourier_space_parameters = {}
