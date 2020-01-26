3fd7bc88b05a2596d449edca0fbdb5ffc961ff1e
Viswanath Pasumarthi <pasumart@buffalo.edu>
2020-01-25 20:27:55 -0500

    update setup.py

--- setup.py
+++ setup.py
@@ -1,53 +1,66 @@
-#!/usr/bin/env python
 """
-This is a setup script to install PyCD
+PyCD
+Python-based Charge Dynamics
+Open-source, cross-platform application supporting lattice-based kinetic Monte Carlo simulations in crystalline systems
 """
+import sys
+from setuptools import setup, find_packages
+import versioneer
 
-import setuptools
+short_description = __doc__.split("\n")
 
-from PyCD import __version__
+# from https://github.com/pytest-dev/pytest-runner#conditional-requirement
+needs_pytest = {'pytest', 'test', 'ptr'}.intersection(sys.argv)
+pytest_runner = ['pytest-runner'] if needs_pytest else []
+
+try:
+    with open("README.md", "r") as handle:
+        long_description = handle.read()
+except:
+    long_description = "\n".join(short_description[2:])
 
 
-if __name__ == "__main__":
-    setuptools.setup(
-        name='PyCD',
-        version=__version__,
-        description='Python-based mesoscale model for Charge Transport',
-        author='Viswanath Pasumarthi',
-        author_email='pasumart@buffalo.edu',
-        url="https://github.com/vpasumarthi/PyCD",
-        packages=setuptools.find_packages(),
-        install_requires=[
-            'scipy',
-            'numpy',
-            'matplotlib',
-            'pyyaml'
-        ],
-        extras_require={
-            'docs': [
-                'sphinx==1.2.3',  # autodoc was broken in 1.3.1
-                'sphinxcontrib-napoleon',
-                'sphinx_rtd_theme',
-                'numpydoc',
-            ],
-            'tests': [
-                'pytest',
-                'pytest-cov',
-                'pytest-pep8',
-                'tox',
-            ],
-        },
+setup(
+    # Self-descriptive entries which should always be present
+    name='PyCD',
+    author='Viswanath Pasumarthi',
+    author_email='pasumart@buffalo.edu',
+    url="https://github.com/vpasumarthi/PyCD",
+    description=short_description[0],
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
+    license='Mozilla Public License 2.0',
+    install_requires=[
+        'scipy',
+        'numpy',
+        'matplotlib',
+        'pyyaml'
+    ],
 
-        tests_require=[
-            'pytest',
-            'pytest-cov',
-            'pytest-pep8',
-            'tox',
-        ],
+    # Which Python importable modules should be included when your package is installed
+    # Handled automatically by setuptools. Use 'exclude' to prevent some specific
+    # subpackage(s) from being added, if needed
+    packages=find_packages(),
 
-        classifiers=[
-            'Development Status :: 4 - Beta',
-            'Intended Audience :: Science/Research',
-            'Programming Language :: Python :: 3',
-        ],
-        zip_safe=True)
+    # Optional include package data to ship with your package
+    # Customize MANIFEST.in if the general case does not suit your needs
+    # Comment out this line to prevent the files from being packaged with your software
+    include_package_data=True,
+
+    # Allows `setup.py test` to work correctly with pytest
+    setup_requires=[] + pytest_runner,
+
+    # Additional entries you may want simply uncomment the lines you want and fill in the data
+    # platforms=['Linux',
+    #            'Mac OS-X',
+    #            'Unix',
+    #            'Windows'],            # Valid platforms your code works on, adjust to your flavor
+    # python_requires=">=3.5",          # Python version restrictions
+
+    # Manual control if final package is compressible or not, set False to prevent the .egg from being made
+    # zip_safe=False,
+
+)
+
